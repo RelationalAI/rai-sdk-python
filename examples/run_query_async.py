12b85f0293ebd999725e245e9e18a4d30324d4cb
Irfan Azam <irfan.azam@relational.ai>
2022-01-27 23:11:02 +0500

    added support for parsing the multipart response

--- examples/run_query_async.py
+++ examples/run_query_async.py
@@ -11,17 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
-
+import json
 from argparse import ArgumentParser
 from urllib.request import HTTPError
 from railib import api, config, show
-
+import pyarrow as pa
 
 def run(database: str, engine: str, command: str, readonly: bool, profile: str):
     cfg = config.read(profile=profile)
     ctx = api.Context(**cfg)
     rsp = api.query_async(ctx, database, engine, command, readonly=readonly)
-    show.results(rsp)
+    result = []
+    content_type_json = b'application/json'
+    content_type_arrow_stream = b'application/vnd.apache.arrow.stream'
+    for part in rsp:
+        header_content_type = part.headers[b'content-type']
+        if header_content_type == content_type_json:
+            result.append(json.loads(bytes(part.content)))
+        elif header_content_type == content_type_arrow_stream:
+            with pa.ipc.open_stream(part.content) as reader:
+                schema = reader.schema
+                batches = [batch for batch in reader]
+                table = pa.Table.from_batches(batches=batches, schema=schema)
+                result.append(table.to_pydict())
+
+    show.results(result, "wire")
 
 
 if __name__ == "__main__":
