be18cad090d5ad1447b762d7d71b3da0484021d9
George Kollias <286415+geokollias@users.noreply.github.com>
2022-02-25 23:29:01 +0200

    Add support for getting transaction metadata

---
+++ examples/get_transaction_metadata.py
@@ -0,0 +1,39 @@
+# Copyright 2021-2022 RelationalAI, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+"""Fetch metadata for the given transaction."""
+
+import json
+from argparse import ArgumentParser
+from urllib.request import HTTPError
+from railib import api, config, show
+
+
+def run(id: str, profile: str):
+    cfg = config.read(profile=profile)
+    ctx = api.Context(**cfg)
+    rsp = api.get_transaction_metadata(ctx, id)
+    print(json.dumps(rsp, indent=2))
+
+
+if __name__ == "__main__":
+    p = ArgumentParser()
+    p.add_argument("-p", "--profile", type=str,
+                   help="profile name", default="default")
+    p.add_argument("id", type=str, help="transaction id")
+    args = p.parse_args()
+    try:
+        run(args.id, args.profile)
+    except HTTPError as e:
+        show.http_error(e)
