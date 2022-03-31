0cb289602f6ee190d61127d47e17624b3532e426
Sameera Salameh <sameera@sameeras-mbp.relational.ai.beta.tailscale.net>
2022-03-31 13:04:25 +0300

    nits

--- railib/api.py
+++ railib/api.py
@@ -448,7 +448,7 @@ class TransactionAsync(object):
             "dbname": self.database,
             "nowait_durable": self.nowait_durable,
             "readonly": self.readonly,
-            "sync_mode": "async"
+            # "sync_mode": "async"
         }
         if self.engine is not None:
             result["engine_name"] = self.engine
@@ -697,18 +697,13 @@ def query(ctx: Context, database: str, engine: str, command: str,
         time.sleep(3)
         txn = get_transaction(ctx, async_result["id"])
         if is_txn_term_state(txn["state"]):
-            txninfo = {
+            rsp.append({
                 "id" : txn["id"],
                 "results_format_version": txn.get("results_format_version", None),
                 "state" : txn["state"]
-            }
-            rsp.append(txninfo)
-
-            metadata = get_transaction_metadata(ctx, txninfo["id"])
-            rsp.append(metadata)
-            
-            results = get_transaction_results_and_problems(ctx, txninfo["id"])
-            rsp.append(results)
+            })
+            rsp.append(get_transaction_metadata(ctx, async_result["id"]))
+            rsp.append(get_transaction_results_and_problems(ctx, async_result["id"]))
             break
 
     return rsp
