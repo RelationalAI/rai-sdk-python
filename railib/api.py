6339abeb8023c13272a954999a618827656f0ca2
Sameera Salameh <sameera@sameeras-mbp.relational.ai.beta.tailscale.net>
2022-07-14 11:44:08 +0300

    get_transaction_problems and get_transaction_metadata to return a list

--- railib/api.py
+++ railib/api.py
@@ -300,7 +300,7 @@ def get_transaction(ctx: Context, id: str) -> dict:
     return _get_resource(ctx, f"{PATH_TRANSACTIONS}/{id}", key="transaction")
 
 
-def get_transaction_metadata(ctx: Context, id: str) -> dict:
+def get_transaction_metadata(ctx: Context, id: str) -> list:
     return _get_collection(ctx, f"{PATH_TRANSACTIONS}/{id}/metadata")
 
 
@@ -308,7 +308,7 @@ def list_transactions(ctx: Context) -> list:
     return _get_collection(ctx, PATH_TRANSACTIONS, key="transactions")
 
 
-def get_transaction_problems(ctx: Context, id: str) -> dict:
+def get_transaction_problems(ctx: Context, id: str) -> list:
     return _get_collection(ctx, f"{PATH_TRANSACTIONS}/{id}/problems")
 
 
