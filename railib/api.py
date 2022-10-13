7d6dd2cb70ae476cfafc7bd309a7e9b01d4aa436
HanyaAdel <hanya.elged@gmail.com>
2022-10-13 17:59:30 +0200

    Revert "added permission"
    This reverts commit 930d7ddf5ef8028586b706edd1be5fa638ce398d.

--- railib/api.py
+++ railib/api.py
@@ -79,8 +79,6 @@ class Permission(str, Enum):
     READ_ACCOUNT = "read:account"
     CREATE_ACCOUNT = "create:account"
     LIST_ACCOUNTS = "list:account"
-    #id providers
-    LIST_ID_PROVIDERS = "list:id_providers"
     # computes
     CREATE_COMPUTE = "create:compute"
     DELETE_COMPUTE = "delete:compute"
