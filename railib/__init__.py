78f9b1b493bebfea68aeb9979703bb63762163b0
helmi.nour <helmiinour@gmail.com>
2023-02-10 13:30:01 +0100

    update changelog && version bump up

--- railib/__init__.py
+++ railib/__init__.py
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version_info__ = (0, 6, 8)
+__version_info__ = (0, 6, 9)
 __version__ = ".".join(map(str, __version_info__))
