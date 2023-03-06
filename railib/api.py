f0c7611df8ab2ef137a86f30d7552abd81c9294a
Helmi Nour <Helmiinour@gmail.com>
2023-03-06 15:33:22 +0100

    Enable logging (#118)
    * logging setup
    
    * add logging for http calls
    
    * fix linter
    
    * read logging configuration from config file
    
    * fix linter
    
    * using package name

--- railib/api.py
+++ railib/api.py
@@ -19,6 +19,7 @@ import pyarrow as pa
 import time
 import re
 import io
+import logging
 from enum import Enum, unique
 from typing import List, Union
 from requests_toolbelt import multipart
@@ -33,6 +34,9 @@ PATH_TRANSACTION = "/transaction"
 PATH_TRANSACTIONS = "/transactions"
 PATH_USER = "/users"
 PATH_OAUTH_CLIENT = "/oauth-clients"
+
+# logger
+logger = logging.getLogger(__package__)
 
 
 # Engine sizes
@@ -887,11 +891,13 @@ def exec(
     readonly: bool = True,
     **kwargs
 ) -> TransactionAsyncResponse:
+    logger.info('exec: database %s engine %s readonly %s' % (database, engine, readonly))
     start_time = time.time()
     txn = exec_async(ctx, database, engine, command, inputs=inputs, readonly=readonly)
     # in case of if short-path, return results directly, no need to poll for
     # state
     if not (txn.results is None):
+        logger.info('transaction id: %s' % txn.transaction["id"])
         return txn
 
     rsp = TransactionAsyncResponse()
@@ -902,6 +908,8 @@ def exec(
         overhead_rate=0.2,
         start_time=start_time,
     )
+
+    logger.info('transaction id: %s' % txn["id"])
 
     rsp.transaction = get_transaction(ctx, txn["id"], **kwargs)
     rsp.metadata = get_transaction_metadata(ctx, txn["id"], **kwargs)
