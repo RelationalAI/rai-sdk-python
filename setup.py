16c5db76b71f85cb4c58858a5416465c43a8604a
helmi.nour <helmiinour@gmail.com>
2023-05-05 12:02:58 +0100

    fix some deps fails for other python version

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="The RelationalAI Software Development Kit for Python",
     install_requires=[
