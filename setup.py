f2decb61271776bdf23c3b89e65351d4abc98d73
helmi.nour <helmiinour@gmail.com>
2023-05-05 11:20:12 +0100

    bump up requests-toolbelt dep version in setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         "ed25519==1.5",
         "pyarrow>=6.0.1",
-        "requests-toolbelt==0.9.1",
+        "requests-toolbelt==1.0.0",
         "protobuf==3.20.2"],
     license="http://www.apache.org/licenses/LICENSE-2.0",
     long_description="Enables access to the RelationalAI REST APIs from Python",
