5900c71e0f8c4d819ecb914658d1345ffb0dd9f3
Ilian Aleixev <akrion@gmail.com>
2024-04-04 11:24:50 -0500

    Fix docs

--- examples/suspend_engine.py
+++ examples/suspend_engine.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License
 
-"""Delete an engine."""
+"""Suspend an engine."""
 
 from argparse import ArgumentParser
 import json
